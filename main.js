450b9aaf014b02b5d41db32878502091fc72565d
Alessio Carrafa <alessio.carrafa@hotmail.it>
2017-03-23 12:43:56 +0100

    package clean for npm publish

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 var LasParser = require('./lasser.js');
-var filename = 'hobu.las';
 
-var parser = new LasParser( filename );
-
+var parser = new LasParser( 'file.las' );
 
 parser.parse().then( () => { console.log( JSON.stringify( parser.pointArray.slice( 0, 5 ), null, 4 ) ); });
